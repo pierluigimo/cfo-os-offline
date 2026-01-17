33200a10c86ed14417892fc15d30a63657b05f26
pierluigimo <pierluigimonaco2@gmail.com>
2026-01-17 06:22:33 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = "cfo-os-offline-v1";
+
+// File da mettere in cache
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./sw.js",
+  "./manifest.json"
+];
+
+// Installazione: salva i file in cache
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Attivazione: pulisce vecchie cache
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch: usa cache se offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).catch(() => cachedResponse);
+    })
+  );
+});
